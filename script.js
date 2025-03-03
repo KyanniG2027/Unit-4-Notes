2ce95fa31008281bb90e6e405929844fe85947fc
KyanniG2027 <kyanni.gonzalez2027@gbwl.org>
2025-03-03 18:40:53 +0000

    started Java

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+/* This is a MULTI-LINE comment 
+The code below is an example STATEMENT
+(instruction/command
+console.log is a FUNCTION that displays info in the console (output) area
+STATEMENTS end with a SEMICOLON(;)
+*/
+
+console.log("HEWO");
+
+//This is a single line comment
+
+//DECLARE a new VARIABLE
+let pokemon;
+
+///ASSIGN a Value to the variable
+// its like putting an item in the box
+pokemon="Snorlax"
