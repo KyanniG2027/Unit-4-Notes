9bfe1e75f88fb8564c00a35621a9676b328c41bb
KyanniG2027 <kyanni.gonzalez2027@gbwl.org>
2025-03-06 18:37:06 +0000

    added more

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ let pokemon;
 ///ASSIGN a Value to the variable
 // its like putting an item in the box
 pokemon="Snorlax"
+
+//DELCARE + ASSIGN in one statement
+let luckyNumber =13;
+
+//"PASS IN" a variable to a Function 
+console.log(luckyNumber)
+
+//CHANGE the value of an existing variable 
+let raining=false; 
+console.log(raining);
+raining = true;
+console.log(raining); 
+
+//COPy data from one variable into another
+let breakfast="starbucks";
+let lunch;
+lunch=breakfast;
+console.log(breakfast);
+console.log(lunch);
